b6dc5ba0393b4f9bb119a4dc7a17583789767d9e
JJPeng <pengjiajun755@163.com>
2022-06-16 00:30:52 +0800

    refactoring-8:将重写的方法，定义为父类的一个函数式接口类型的成员变量，委托给父类执行子类想要执行的方法，那么也就不再需要重写父类的方法了

--- src/main/java/com/jjpeng/args/IntOptionParser.java
+++ src/main/java/com/jjpeng/args/IntOptionParser.java
@@ -2,8 +2,7 @@ package com.jjpeng.args;
 
 class IntOptionParser extends StringOptionParser {
 
-    @Override
-    protected Object parseValue(String value) {
-        return Integer.parseInt(value);
+    public IntOptionParser() {
+        super(Integer::parseInt);
     }
 }
--- src/main/java/com/jjpeng/args/StringOptionParser.java
+++ src/main/java/com/jjpeng/args/StringOptionParser.java
@@ -1,16 +1,22 @@
 package com.jjpeng.args;
 
 import java.util.List;
+import java.util.function.Function;
 
 class StringOptionParser implements OptionParser {
+    Function<String, Object> valueParser = String::valueOf;
+
+    public StringOptionParser() {
+    }
+
+    public StringOptionParser(Function<String, Object> valueParser) {
+        this.valueParser = valueParser;
+    }
+
     @Override
     public Object parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
         String value = arguments.get(index + 1);
-        return parseValue(value);
-    }
-
-    protected Object parseValue(String value) {
-        return String.valueOf(value);
+        return valueParser.apply(value);
     }
 }
