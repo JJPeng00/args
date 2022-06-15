0995dfc88a9d15a357cdd9eed90a6f26a7ead413
JJPeng <pengjiajun755@163.com>
2022-06-15 23:34:07 +0800

    refactoring-7:提出公共方法后使IntOptionParser继承StringOptionParser并重写parseValue方法，就达到了重用方法的目的

--- src/main/java/com/jjpeng/args/IntOptionParser.java
+++ src/main/java/com/jjpeng/args/IntOptionParser.java
@@ -1,12 +1,9 @@
 package com.jjpeng.args;
 
-import java.util.List;
+class IntOptionParser extends StringOptionParser {
 
-class IntOptionParser implements OptionParser {
     @Override
-    public Object parse(List<String> arguments, Option option) {
-        int index = arguments.indexOf("-" + option.value());
-        String value = arguments.get(index + 1);
+    protected Object parseValue(String value) {
         return Integer.parseInt(value);
     }
 }
