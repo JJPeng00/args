e3916991464e98f7cb58fed8fee8db0291a8aaf9
JJPeng <pengjiajun755@163.com>
2022-06-19 15:37:26 +0800

    refactoring:将if语句中的判断条件换用其他更直观的方式实现

--- src/main/java/com/jjpeng/args/SingleValueOptionParser.java
+++ src/main/java/com/jjpeng/args/SingleValueOptionParser.java
@@ -2,6 +2,7 @@ package com.jjpeng.args;
 
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 
 class SingleValueOptionParser<T> implements OptionParser<T> {
     Function<String, T> valueParser;
@@ -17,24 +18,19 @@ class SingleValueOptionParser<T> implements OptionParser<T> {
         int index = arguments.indexOf("-" + option.value());
         if (index == -1) return defaultValue;
 
-        if (isReachEndOfList(arguments, index) ||
-                isFollowedByOtherFlag(arguments, index)) throw new InsufficientArgumentException(option.value());
+        List<String> values = getValues(arguments, index);
 
-        if (nextArgumentIsNotAFlag(arguments, index)) throw new TooManyArgumentException(option.value());
+        if (values.size() < 1) throw new InsufficientArgumentException(option.value());
+
+        if (values.size() > 1) throw new TooManyArgumentException(option.value());
 
         return valueParser.apply(arguments.get(index + 1));
     }
 
-    private boolean nextArgumentIsNotAFlag(List<String> arguments, int index) {
-        return index + 2 < arguments.size() &&
-                !arguments.get(index + 2).startsWith("-");
-    }
-
-    private boolean isFollowedByOtherFlag(List<String> arguments, int index) {
-        return arguments.get(index + 1).startsWith("-");
-    }
-
-    private boolean isReachEndOfList(List<String> arguments, int index) {
-        return index + 1 == arguments.size();
+    private List<String> getValues(List<String> arguments, int index) {
+        int followingFlag = IntStream.range(index + 1, arguments.size())
+                .filter(it -> arguments.get(it).startsWith("-"))
+                .findFirst().orElse(arguments.size());
+        return arguments.subList(index + 1, followingFlag);
     }
 }
